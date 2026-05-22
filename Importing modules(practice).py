306d6ca7a95c0382a27cc58c7d885ef04bdfe671
mimi73727170-coder <mimi73727170@gmail.com>
2026-05-22 19:44:19 +0530

    Add files via upload

---
+++ Importing modules(practice).py
@@ -0,0 +1,22 @@
+import math
+print (math.sqrt(25))
+
+import random
+print (random.randint(1,10))
+
+import time
+print("Start")
+time.sleep(3)
+print("End")
+
+import math 
+print(math.sqrt(81))
+
+import random
+print(random.randint(1,100))
+
+def lunky () :
+    print("Lunky")
+
+import random
+print(random.choice(["apple","mango","banana"]))
